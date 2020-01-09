8c1c948dfe98c8e14ae33b05eae4fa9934fda919
EIMAR QUINN <G00374109@gmit.ie>
2020-01-09 14:39:38 +0000

    Add project files.

---
+++ JavaCalculator/JavaCalculator/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace JavaCalculator
+{
+   
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnCalculate_Clicked(object sender, EventArgs e)
+        {
+            float M1 = (float)Convert.ToDecimal(MCQ1.Text);
+            float M2 = (float)Convert.ToDecimal(MCQ2.Text);
+            float LabEx = (float)Convert.ToDecimal(LabExam.Text);
+            float Attend = (float)Convert.ToDecimal(Attendance.Text);
+
+            float overallGrade = (((M1 / 20) + (M2 / 20) + (LabEx / 50) + (Attend / 10)) * 100) / 4;
+            TotalGrade.Text = "Your overall grade is: " + overallGrade.ToString();
+
+        }
+    }
+}
